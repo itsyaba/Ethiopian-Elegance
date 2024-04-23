1d5ff951a421e250a8ee5d59e98c79b54b2f6a30
Yeabsira Tarekegn <yabahane@gmail.com>
2024-04-23 23:50:50 +0300

    fix: backend error

--- frontend/src/constants.js
+++ frontend/src/constants.js
@@ -1,6 +1,8 @@
-// export const BASE_URL = process.env.NODE_ENV === "development" ? "http://localhost:5000" : "";
+export const BASE_URL =
+  process.env.NODE_ENV === "development"
+    ? "http://localhost:5000"
+    : "https://ethiopian-elegance.onrender.com/";
 
-export const BASE_URL = "http://localhost:5000"; // If using proxy
 export const PRODUCTS_URL = "/api/products";
 export const USERS_URL = "/api/users";
 export const ORDERS_URL = "/api/orders";
